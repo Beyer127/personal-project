87f7bb0075fef815cb84e79359169a76f46842d8
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-19 03:15:37 -0800

    code clean up and un needed files deleted

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import store from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById("root")
 );
